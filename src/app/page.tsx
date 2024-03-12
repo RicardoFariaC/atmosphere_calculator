ee21e098449306cce3952c3fbd0c4c0430ffd7b1
RicardoFariaC <ricardofcosta0804@gmail.com>
2024-03-12 16:44:28 -0300

    init: First Commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,146 @@
-import Image from "next/image";
+"use client"
+
+import { invoke } from "@tauri-apps/api/tauri"
+import { Calculator, FileQuestion, LineChart, Table } from "lucide-react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [altitude, setAltitude] = useState<number>(0);
+  // const [greeting, setGreeting] = useState<string>("");
+
+  // useEffect(() => {
+  //   console.log("ola, mundo")
+  //   invoke<string>("greet", { name: "next"})
+  //     .then(res => setGreeting(res))
+  //     .catch(console.error);
+  // }, [])
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <div className="flex h-screen flex-col">
+      <div className="flex flex-1">
+        <aside className="w-auto">
+          <nav className="bg-zinc-900 space-y-2 sticky top-3 p-6 m-3 rounded-md">
+            <a href="" className="flex items-center gap-2 hover:bg-slate-300 hover:bg-opacity-20 py-4 px-2 rounded-md">
+              <Calculator />
+              Calculator
+            </a>
+            <a href="" className="flex items-center gap-2 hover:bg-slate-300 hover:bg-opacity-20 py-4 px-2 rounded-md">
+              <Table />
+              Table
+            </a>
+            <a href="" className="flex items-center gap-2 hover:bg-slate-300 hover:bg-opacity-20 py-4 px-2 rounded-md">
+              <LineChart />
+              Graphs
+            </a>
+            <a href="" className="flex items-center gap-2 hover:bg-slate-300 hover:bg-opacity-20 py-4 px-2 rounded-md">
+              <FileQuestion />
+              Help
+            </a>
+          </nav>
+        </aside>
+        <main className="flex-1 p-6">
+          <p className="pb-2">Input</p>
+          <div className="w-auto p-6 rounded-md bg-slate-100 bg-opacity-20 drop-shadow-lg shadow-lg">
+            <form className="flex flex-col gap-4 text-lg">
+              <div className="flex items-center gap-4 flex-1">
+                <label htmlFor="unit" className="basis-1/2">Unit System</label>
+                <select name="" id="" disabled={true} className="bg-zinc-800 p-2 rounded-md w-64 basis-1/2">
+                  <option value="is">International System</option>
+                </select>
+              </div>
+              <div className="flex items-center gap-4 flex-1">
+                <label htmlFor="altitude" className="basis-1/2">Altitude</label>
+                <input
+                  value={altitude}
+                  type="number" 
+                  id="altitude" 
+                  className="bg-zinc-800 p-2 rounded-md basis-1/4"
+                  />
+                <select className="bg-zinc-800 p-2 rounded-md w-64 basis-1/4">
+                  <option value="1">meters[m]</option>
+                  <option value="1000">kilometers[km]</option>
+                  <option value="0.3048">feet[ft]</option>
+                </select>
+              </div>
+              <div className="flex items-center gap-4 flex-1">
+                <label htmlFor="velocity" className="basis-1/2">Velocity</label>
+                <input 
+                  type="number" 
+                  id="velocity" 
+                  className="bg-zinc-800 p-2 rounded-md basis-1/4"
+                  />
+                <select className="bg-zinc-800 p-2 rounded-md w-64 basis-1/4">
+                  <option value="1">meters/second[m/s]</option>
+                  <option value="0.2777778">kilometers/hour[km/h]</option>
+                  <option value="0.3048">feet/second[ft/s]</option>
+                </select>
+              </div>
+              <div className="flex items-center gap-4 flex-1">
+                <label htmlFor="refLength" className="basis-1/2">Reference Length</label>
+                <input 
+                  type="number" 
+                  id="refLength" 
+                  className="bg-zinc-800 p-2 rounded-md basis-1/4"
+                  />
+                <select className="bg-zinc-800 p-2 rounded-md w-64 basis-1/4">
+                  <option value="1">meters[m]</option>
+                  <option value="1000">kilometers[km]</option>
+                  <option value="0.3048">feet[ft]</option>
+                </select>
+              </div>
+              <div className="flex items-center gap-4 flex-1">
+                <label htmlFor="temperature" className="basis-1/2">Temperature offset</label>
+                <input 
+                  type="number" 
+                  id="temperature"
+                  className="bg-zinc-800 p-2 rounded-md basis-1/4" 
+                />
+                <select className="bg-zinc-800 p-2 rounded-md w-64 basis-1/4" >
+                  <option value="1">Celsius[°C] / Kelvin[K]</option>
+                  <option value="0.5556">Fahrenheit[°F] / Rankine[°R]</option>
+                </select>
+              </div>
+              <div className="flex items-center justify-end gap-4 h-12">
+                <button className="px-4 py-1 bg-zinc-600 drop-shadow-md shadow-md rounded-md focus:outline">Compute</button>
+                <button className="px-4 py-1 bg-zinc-600 drop-shadow-md shadow-md rounded-md focus:outline">Clear</button>
+              </div>
+            </form>
+          </div>
+          <p className="pt-4 pb-2">Output</p>
+          <div className="w-auto p-6 rounded-md bg-slate-100 bg-opacity-20 drop-shadow-lg shadow-lg">
+            <form className="flex flex-col gap-4 text-lg">
+              <div className="flex items-center gap-4 flex-1 ">
+                <label htmlFor="geoaltitude" className="basis-1/2">Geometric Altitude</label>
+                <input
+                  disabled
+                  type="number" 
+                  id="geoaltitude" 
+                  className="bg-zinc-800 p-2 rounded-md basis-1/4"
+                  />
+                <select className="bg-zinc-800 p-2 rounded-md w-64 basis-1/4">
+                  <option value="1">meters[m]</option>
+                  <option value="1000">kilometers[km]</option>
+                  <option value="0.3048">feet[ft]</option>
+                </select>
+              </div>
+              <div className="flex items-center gap-4 flex-1">
+                <label htmlFor="temperature" className="basis-1/2">Geopotential Altitude</label>
+                <input 
+                  disabled
+                  type="number" 
+                  id="temperature"
+                  className="bg-zinc-800 p-2 rounded-md basis-1/4" 
+                />
+                <select className="bg-zinc-800 p-2 rounded-md w-64 basis-1/4">
+                  <option value="1">meters[m]</option>
+                  <option value="1000">kilometers[km]</option>
+                  <option value="0.3048">feet[ft]</option>
+                </select>
+              </div>
+            </form>
+          </div>
+        </main>
+      </div >
+    </div>
   );
 }
