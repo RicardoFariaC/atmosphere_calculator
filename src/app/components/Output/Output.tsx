a90d2c439d947a2d9dc5f810125f20e4a14727f8
RicardoFariaC <ricardofcosta0804@gmail.com>
2024-05-14 13:05:12 -0300

    refactor(output): create new output folder

---
+++ src/app/components/Output/Output.tsx
@@ -0,0 +1,57 @@
+"use client"
+import { ReactNode, ReactPropTypes, useEffect, useState } from "react";
+import { SelectOption } from "./Select/SelectOptions";
+import { Unit } from "@/app/utils/units";
+
+interface propTypes {
+    label: string,
+    value: number,
+    unit: Unit[],
+    fixed?: number,
+}
+
+export const Output = ({ label, value, unit, fixed = 2}: propTypes) => {
+    const [optionUnit, setOptionUnit] = useState<string>(unit.find((unitValue) => parseFloat(unitValue.value) == 1)?.unit || "");
+    const [selectValueUnit, setSelectValueUnit] = useState<number>(1);
+
+    useEffect(() => {
+        setSelectValueUnit(parseFloat(unit.find((unitValue) => unitValue.unit == optionUnit)?.value || "1"));
+    }, [optionUnit])
+
+    function toCapitalize(label: string): string {
+        return [label.at(0)?.toUpperCase() + "" + label.slice(1)].join("");
+    } 
+
+    return (
+        <div className="flex items-center gap-4 flex-1 ">
+            <label htmlFor={label} className="basis-1/2">{toCapitalize(label)}</label>
+            <input
+                disabled
+                value={parseFloat((value / (selectValueUnit || 1)).toFixed(fixed)) ? (value / (selectValueUnit || 1)).toFixed(fixed) : 0}
+                id={label} 
+                className="bg-zinc-800 p-2 rounded-md basis-1/4"
+            />
+            <SelectOption
+                value={optionUnit}
+                setValue={
+                    (e) => {
+                        setOptionUnit(e as string);
+                        setSelectValueUnit(parseFloat(unit.find((unitValue) => unitValue.unit == optionUnit)!.value));
+                    }
+                }
+            >
+                {unit.map((unitValue) => {
+                        if(!!optionUnit) {
+                            return ( 
+                                <option
+                                key={unitValue.name} value={unitValue.unit}
+                                >
+                                    {`${unitValue.name}[${unitValue.unit}]`}
+                                </option>
+                            )
+                        }
+                })}
+            </SelectOption>
+        </div>
+    )
+};
