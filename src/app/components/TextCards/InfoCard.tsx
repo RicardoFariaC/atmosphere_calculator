a51f309a30ccd1ed9278d3d017a6698e4b084b00
RicardoFariaC <ricardofcosta0804@gmail.com>
2024-05-14 13:06:07 -0300

    feat(ui): create info card
    info cards will be necessary to label sections in ui

---
+++ src/app/components/TextCards/InfoCard.tsx
@@ -0,0 +1,16 @@
+import { ReactNode } from "react";
+
+interface InfoCardPropTypes {
+    className: string;
+    label: string
+}
+
+export const InfoCard = ({className, label}: InfoCardPropTypes) => {
+    return (
+            <p 
+                className={`bg-slate-100 bg-opacity-40 rounded-md text-black font-bold py-1 px-1 ${className}`}
+            >
+                {label}
+            </p>
+    )
+}
